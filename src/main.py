e02574ce9e880d3a885902d40ca19ea5adf56ad9
Timur YodZ0 <tima-mustafa@mail.ru>
2024-10-11 10:50:15 +0600

    Create main file

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import dearpygui.dearpygui as dpg
+
+
+# DEFAULT
+dpg.create_context()
+
+# SOME CODE
+
+# DEFAULT
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
