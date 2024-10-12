2e49e25780baf3d017e1e9e9026c959d3983c656
Timur YodZ0 <tima-mustafa@mail.ru>
2024-10-12 06:56:43 +0600

    Create charset generator

--- src/utils/pass_generator.py
+++ src/utils/pass_generator.py
@@ -1,5 +1,20 @@
 import secrets
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 
-def generate_password(length: int, charset: list[str]):
+def get_charset(nums: bool, chars: bool, capitals: bool, specific: bool) -> str:
+    charset = ""
+    if nums is True:
+        charset += digits
+    if chars is True:
+        charset += ascii_lowercase
+    if capitals is True:
+        charset += ascii_uppercase
+    if specific is True:
+        charset += punctuation
+
+    return charset
+
+
+def password_generator(length: int, charset: str) -> str:
     return "".join(secrets.choice(charset) for _ in range(length))
